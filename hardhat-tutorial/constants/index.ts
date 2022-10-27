c295bd3300e046e4f791e4ccb1886a2f8ebffae2
Maruf Sharifi <maruf@truetale.io>
2022-10-27 22:07:01 +0430

    get my contract ready to deploy

---
+++ hardhat-tutorial/constants/index.ts
@@ -0,0 +1,8 @@
+
+// Address of the Whitelist Contract that you deployed
+export const WHITELIST_CONTRACT_ADDRESS = "0xCA87e63f7A96c78a9653c52D0d7d498CA20CCe1f";
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+
+// 0xFFdE2ec0fF0543f1ACA6A3F02494FeC3762eE768
--- hardhat-tutorial/scripts/deploy.ts
+++ hardhat-tutorial/scripts/deploy.ts
@@ -1,23 +1,38 @@
 import { ethers } from "hardhat";
+import dotenv from 'dotenv'
+import {WHITELIST_CONTRACT_ADDRESS, METADATA_URL} from '../constants'
+dotenv.config({path: ".env"})
+
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+   // Address of the whitelist contract that you deployed in the previous module
+   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+   // URL from where we can extract the metadata for a Crypto Dev NFT
+   const metadataURL = METADATA_URL;
+   /*
+   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+   so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+   */
+   const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+ 
+   // deploy the contract
+   const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+     metadataURL,
+     whitelistContract
+   );
+ 
+   // print the address of the deployed contract
+   console.log(
+     "Crypto Devs Contract Address:",
+     deployedCryptoDevsContract.address
+   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
