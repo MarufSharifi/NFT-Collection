715246cbcc57970322eeba996b831ed57c02023b
Maruf Sharifi <maruf@truetale.io>
2022-10-28 10:39:39 +0430

    added meta-data url to my hardhat project

--- hardhat-tutorial/constants/index.ts
+++ hardhat-tutorial/constants/index.ts
@@ -2,6 +2,6 @@
 // Address of the Whitelist Contract that you deployed
 export const WHITELIST_CONTRACT_ADDRESS = "0xCA87e63f7A96c78a9653c52D0d7d498CA20CCe1f";
 // URL to extract Metadata for a Crypto Dev NFT
-export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+export const METADATA_URL = "https://nft-collection-omega-weld.vercel.app/api/";
 
 
