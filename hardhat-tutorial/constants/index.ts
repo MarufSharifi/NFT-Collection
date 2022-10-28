08f1699d7a5e223c88b3ad0cbdb193ebc5bddcce
Maruf Sharifi <maruf@truetale.io>
2022-10-28 10:18:53 +0430

    added our contract api and contract address to our web-app

--- hardhat-tutorial/constants/index.ts
+++ hardhat-tutorial/constants/index.ts
@@ -5,4 +5,3 @@ export const WHITELIST_CONTRACT_ADDRESS = "0xCA87e63f7A96c78a9653c52D0d7d498CA20
 export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 
-// 0xFFdE2ec0fF0543f1ACA6A3F02494FeC3762eE768
